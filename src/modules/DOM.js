1ecde15d8242d88ed4ae87d68d8ac304c97a7bf0
fionicss <118982867+Fionicss@users.noreply.github.com>
2024-05-20 18:21:40 +0800

    Added dialog and DOM interation to HTML and DOM.js for new project button.

--- src/modules/DOM.js
+++ src/modules/DOM.js
@@ -1,13 +1,41 @@
 import { projectBuilder, projectLibrary } from "./objectBuilder";
 
-const newProject = (function() {
-    document.getElementById('newProject').addEventListener('click', () => build());
+const newProjectBtn = document.getElementById('newProject');
+const projectHeader = document.getElementById('projectHeader');
+const projectDialog = document.getElementById('projectDialog');
+const form = document.querySelector('form');
+const projectName = document.getElementById('projectName');
+const submitBtn = document.getElementById('submit')
+const cancelBtn = document.getElementById('cancel')
 
-    function build(){
-        var name = prompt('Project Name')
-        projectBuilder(name);
+const newProject = (function() {
+    newProjectBtn.addEventListener('click', openProjectDialog);
+    submitBtn.addEventListener('click', buildProject);
+    cancelBtn.addEventListener('click', cancelDialog);
+    
+    function openProjectDialog() {
+        projectDialog.open = true;
+        form.reset();
+    };
+
+    function buildProject(){
+        var name = projectName.value;
+        projectBuilder(name); 
+        appendProject(name);
     // Remove when done
         console.log(projectLibrary)
+    };
+
+    function appendProject(name){
+        const projectText = document.createElement('button');
+        projectText.textContent = `${name}`;
+        projectText.setAttribute('id', name);
+        projectHeader.insertBefore(projectText, newProjectBtn);
+    };
+
+    function cancelDialog(event){
+        event.preventDefault();
+        projectDialog.close();
     };
 
 })();

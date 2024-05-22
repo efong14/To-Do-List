02aa32005244906bc15abf381c11242275415a55
fionicss <118982867+Fionicss@users.noreply.github.com>
2024-05-22 17:52:57 +0800

    Added variable to track selected project. Added add new note button to note content area per project.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './style.css'
-import { newProject } from './modules/DOM';
+import { DOMCreator } from './modules/DOM';
 
-// Next Item: Add modal to get data for new project builder.
 
 // console.log(projectLibrary);
 
--- src/modules/DOM.js
+++ src/modules/DOM.js
@@ -1,18 +1,26 @@
-import { projectBuilder, projectLibrary } from "./objectBuilder";
+import { noteBuilder, projectBuilder, projectLibrary } from "./objectBuilder";
 
 const form = document.querySelector('form');
 const noteContainer = document.getElementById('noteContainer');
 const newProjectBtn = document.getElementById('newProject');
 const projectHeader = document.getElementById('projectHeader');
 const projectDialog = document.getElementById('projectDialog');
+const noteDialog = document.getElementById('noteDialog');
 const projectName = document.getElementById('projectName');
-const submitBtn = document.getElementById('submit')
-const cancelBtn = document.getElementById('cancel')
+const noteTitle = document.getElementById('noteTitle');
+const noteDescription = document.getElementById('noteDescription');
+const noteDueDate = document.getElementById('noteDueDate');
+const notePriority = document.getElementById('notePriority');
+const projectSubmitBtn = document.getElementById('projectSubmit')
+const noteSubmitBtn = document.getElementById('noteSubmit')
+const cancelBtn = document.querySelectorAll('#cancel')
+
+var selectedProject = ''
 
 const DOMCreator = (function() {
     newProjectBtn.addEventListener('click', openProjectDialog);
-    submitBtn.addEventListener('click', buildProject);
-    cancelBtn.addEventListener('click', cancelDialog);
+    projectSubmitBtn.addEventListener('click', buildProject);
+    cancelBtn.forEach((button) => button.onclick = () => cancelDialog);
     
     function openProjectDialog() {
         projectDialog.open = true;
@@ -26,9 +34,8 @@ const DOMCreator = (function() {
 
     function buildProject(){
         if (projectName.value == ''){return};
-        var name = projectName.value;
-        projectBuilder(name); 
-        appendProject(name);
+        projectBuilder(projectName.value); 
+        appendProject(projectName.value);
     // Remove when done
         console.log(projectLibrary)
     };
@@ -38,20 +45,45 @@ const DOMCreator = (function() {
         projectBtn.textContent = `${name}`;
         projectBtn.setAttribute('id', name);
         projectHeader.insertBefore(projectBtn, newProjectBtn);
-        projectBtn.addEventListener('click', newNote.noteDisplay);
+        projectBtn.addEventListener('click', noteDisplaySetter);
+
+        function noteDisplaySetter() {
+            selectedProject = name;
+            newNote.noteDisplay();
+        }
     };
 })();
 
 const newNote = (function () {
-    const noteDisplay = () => {
-        const noteInstance = document.createElement('div');
-        const newNoteBtn = document.createElement('button');
-        noteContainer.textContent = '';
-        noteInstance.textContent = `${projectLibrary[projectName.value]}`;
-        noteContainer.appendChild(noteInstance);
+    const noteInstance = document.createElement('div');
+    const newNoteBtn = document.createElement('button');
 
+    noteSubmitBtn.addEventListener('click', buildNote)
+
+    function openNoteDialog() {
+        noteDialog.open = true;
+        form.reset();
     };
-    return {noteDisplay}
+
+    function buildNote() {
+        const note = noteBuilder(noteTitle.value, noteDescription.value, noteDueDate.value, notePriority.value);
+        note.noteAdder(selectedProject);
+        noteInstance.textContent = `${projectLibrary[selectedProject]}`;
+    }
+
+    const noteDisplay = () => {
+        newNoteBtn.textContent = '+'
+        newNoteBtn.addEventListener('click', openNoteDialog)
+        noteContainer.textContent = '';
+        // noteInstance.textContent = `${projectLibrary[selectedProject]}`; REMOVE
+        noteContainer.appendChild(noteInstance);
+        noteContainer.appendChild(newNoteBtn);
+    };
+
+    // Make New Function that loops through each of the selected project's objects and renders its title in the noteInstance field.
+
+    const displayTest = () => {console.log('Works!')}
+    return {noteDisplay, displayTest}
 })();
 
 export {DOMCreator}
